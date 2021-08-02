10d61ab34c30aa2031dfadd51d06f5e16f11128b
Hrithik Biswas <hrithikpersonalid@gmail.com>
2021-08-02 22:56:52 +0600

    Configuration for single entry

---
+++ README.md
@@ -0,0 +1,6 @@
+# webpack5-basic-configuration
+
+1. Main branch -- Configuration for single entry
+2. Multiple-entry branch -- Configuration for multiple entry
+
+In this configuration, dist folder will be created and link up file automatically.
---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    entry: "./src/js/index.js",
+    output: {
+        filename: "./js/main.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+    },
+    mode: "development",
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        compress: true,
+        port: 5050,
+        open: true,
+    },
+    devtool: "source-map",
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: "babel-loader",
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: "css-loader",
+                        options: {
+                            esModule: false,
+                        },
+                    },
+                    "postcss-loader",
+                    "sass-loader",
+                ],
+            },
+            {
+                test: /\.html$/,
+                loader: "html-loader",
+                options: {
+                    esModule: false,
+                },
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                loader: "file-loader",
+                options: {
+                    name: "[name].[ext]",
+                    outputPath: "images",
+                    esModule: false,
+                },
+            },
+            {
+                test: /\.(mp4|webm|wmv)$/i,
+                loader: "file-loader",
+                options: {
+                    name: "[name].[ext]",
+                    outputPath: "Video",
+                },
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+            inject: "body",
+        }),
+        new MiniCssExtractPlugin({
+            filename: "./css/style.bundle.css",
+            chunkFilename: "./css/[id].css",
+        }),
+    ],
+};
